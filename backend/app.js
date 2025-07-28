8544edca15e9b999471bd2572f70e25b5cb42848
Sohaila Hesham <sohaila.hesham456@gmail.com>
2025-07-28 16:09:49 +0300

    Update app entry and Swagger API documentation

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+require("./jobs/deleteOldUsers");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
-const adminRouter = require("./routes/adminRoutes");
+const exerciseRouter = require("./routes/exerciseRoute");
+const workoutRouter = require("./routes/workoutRoutes");
+const profileRouter = require("./routes/profileRoutes");
 const passportSetup = require("./config/passport-setup");
 const verifyToken = require("./middlewares/verifyToken");
 const allowedTo = require("./middlewares/allowedTo");
@@ -14,12 +17,13 @@ const cors = require("cors");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const { limiter } = require("./middlewares/rateLimit");
 // Generate a random hexadecimal number for jwt secret key
 // const randomNumber = crypto.randomBytes(64).toString("hex", 16);
 // console.log(randomNumber);
 
 const app = express();
-
+// console.log(new Date(Date.now() - 31 * 24 * 60 * 60 * 1000));
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -30,6 +34,8 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+//console.log("JWT Secret:", process.env.ACCESS_TOKEN_KEY);
+app.use(limiter);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -39,7 +45,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:5173", // Adjust based on frontend URL
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -55,7 +65,9 @@ app.get("/about", verifyToken, allowedTo("ADMIN"), (req, res) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
-app.use("/api/dashboard", adminRouter);
+app.use("/api/exercises", exerciseRouter);
+app.use("/api/workouts", workoutRouter);
+app.use("/api/profile", profileRouter);
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
